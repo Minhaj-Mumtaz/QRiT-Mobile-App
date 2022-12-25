9b0f5d92e24b7d8c5ba35f3accbd5de167c07248
Arsalan Warsi <arsalanwarsi27@gmail.com>
2022-12-25 16:14:28 +0500

    QRiT #time 5m #comment vendor promotions view screen complete

--- service/Config.js
+++ service/Config.js
@@ -5,6 +5,6 @@ import AuthService from "./AuthService";
 export const config = axios.create({
   // baseURL: "http://172.20.10.4:3000/"
   // baseURL: "http://192.168.10.8:3000/"
-  baseURL: "http://192.168.10.4:3000/"
+  baseURL: "http://192.168.10.10:3000/"
   // baseURL:"http://10.23.43.178:3000/"
 });
